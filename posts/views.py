1b3dbd91707473f3e50a63bc12e9b5b8f8425f29
EJDiamond <emilydiamond321@gmail.com>
2023-01-14 16:27:42 +0000

    Add post detail view so the owners of a post are able to edit and delete it

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
+    """
+    Create lists of posts which are read only if the user is not authenticated
+    """
     serializer_class = PostSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
@@ -31,3 +36,58 @@ class PostList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class PostDetail(APIView):
+    """
+    Only posts owner can edit or delete it
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        """
+        Raises error if the post doesn't exist
+        """
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        """
+        Retrieve post by id
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Makes it possible to edit post
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        """ Makes it possible for owner to delete a post
+        """
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+

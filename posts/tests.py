60f124490c5dc5d2718d68e4ab0825f500a89cf8
EJDiamond <emilydiamond321@gmail.com>
2023-01-14 17:13:14 +0000

    Add PostDetail test to ensure users can only retrieve posts with a vlaid id and can only edit their own posts

--- posts/tests.py
+++ posts/tests.py
@@ -30,3 +30,36 @@ class PostListViewTest(APITestCase):
     def test_logged_out_user_can_not_create_post(self):
         response = self.client.post('/posts/', {'plant': 'a plant'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        tom = User.objects.create_user(username='tom', password='test')
+        george = User.objects.create_user(username='george', password='test')
+        Post.objects.create(
+            owner=tom, plant='a plant', question='toms plant'
+        )
+        Post.objects.create(
+            owner=george, plant='a cactus', question='georges plant'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['plant'], 'a plant')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_can_not_fetch_post_with_invalid_id(self):
+        response = self.client.get('/posts/4/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_edit_own_post(self):
+        self.client.login(username='tom', password='test')
+        response = self.client.put('/posts/1/', {'plant': 'new plant'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.plant, 'new plant')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_can_not_edit_another_users_post(self):
+        self.client.login(username='george', password='test')
+        response = self.client.put('/posts/1/', {'plant': 'new plant'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
